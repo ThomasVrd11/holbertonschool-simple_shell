449b7cd7f9927ca0b33318c2d49783cc7bf4f143
ThomasVrd11 <Thomasviard@orange.fr>
2023-12-11 09:51:54 +0100

    creating an preset header file!

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef _SHELL_MAIN_HEADER_
+#define _SHELL_MAIN_HEADER_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <stddef.h>
+#include <string.h>
+
+#endif
