600fe33b4254ffbaf32db59c1c9ab9755f2b983e
root <Part4@github.com>
2023-12-12 07:10:37 -0800

    adding correction for betty

--- input.c
+++ input.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * Functions for input handling
+ * read_user_input - chaussure
+ * Return: Chaussure
  */
 
 char *read_user_input()
 {
 	char *input = NULL;
 	size_t buffer_size = 0;
+
 	getline(&input, &buffer_size, stdin);
 	return (input);
 }
