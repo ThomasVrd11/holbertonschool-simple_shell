90eb6fc8daf6b8db5ebb7be16a052362466181a4
ThomasVrd11 <Thomasviard@orange.fr>
2023-12-12 21:50:29 +0100

    input.c was buggy, merger error

--- input.c
+++ input.c
@@ -9,7 +9,6 @@ char *read_user_input()
 {
 	char *input = NULL;
 	size_t buffer_size = 0;
-<<<<<<< HEAD
 	ssize_t characters;
 
 	characters = getline(&input, &buffer_size, stdin);
@@ -18,9 +17,6 @@ char *read_user_input()
 		free(input);
 		exit(EXIT_FAILURE);
 	}
-=======
-
 	getline(&input, &buffer_size, stdin);
->>>>>>> 600fe33b4254ffbaf32db59c1c9ab9755f2b983e
 	return (input);
 }
