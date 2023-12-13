3b1454f30ae4264ac97897807b73c3aeb6654074
root <Part4@github.com>
2023-12-13 04:19:44 -0800

    changes for betty

--- utility.c
+++ utility.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
- * Utility function for string manipulation or various tasks
-*/
+ * is_empty - chaussure
+ * @str: chaussure
+ * Return: chaussure
+ */
 
 
 int is_empty(const char *str)
 {
-    while (*str != '\0')
-    {
-        if (!isspace((unsigned char) *str))
-        return (0);
-        str++;
-    }
-    return (1);
+	while (*str != '\0')
+	{
+		if (!isspace((unsigned char) *str))
+			return (0);
+		str++;
+	}
+	return (1);
 }
