78739c5b207fd8c2072ab19b55f45f8f5f2b33bd
ThomasVrd11 <Thomasviard@orange.fr>
2023-12-18 14:34:44 +0100

    better error message

--- executor.c
+++ executor.c
@@ -61,7 +61,7 @@ void execute_command(char **parsed_command, char *program_name, char **env)
 
 	if (!cmd_path)
 	{
-		fprintf(stderr, "%s: %s: not found\n", program_name, parsed_command[0]);
+		fprintf(stderr, "%s: 1: %s: not found\n", program_name, parsed_command[0]);
 		return;
 	}
 
